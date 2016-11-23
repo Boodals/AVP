3fee2353a3fb693714705e958c098dd180085a5c
Boodals <HarryBarryBoodals@Yahoo.co.uk>
2016-11-23 12:12:44 +0000

    Improvements to Path system

--- Assets/Scripts/Editor/PathEditor.cs
+++ Assets/Scripts/Editor/PathEditor.cs
@@ -5,7 +5,7 @@ using System.Collections;
 [CustomEditor(typeof(Path))]
 public class PathEditor : Editor
 {
-
+	
 	private Path path;
 
 	private SerializedProperty nodesArrayProp;
@@ -20,10 +20,10 @@ public class PathEditor : Editor
 		{
 			nodesArrayProp.arraySize = 4;
 
-			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 4).FindPropertyRelative("position").vector3Value = new Vector3(0f, 0f, 0f);
-			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 3).FindPropertyRelative("position").vector3Value = new Vector3(1f, 0f, 1f);
-			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 2).FindPropertyRelative("position").vector3Value = new Vector3(-1f, 0f, 3f);
-			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 1).FindPropertyRelative("position").vector3Value = new Vector3(0f, 0f, 4f);
+			nodesArrayProp.GetArrayElementAtIndex(0).FindPropertyRelative("position").vector3Value = new Vector3(0f, 0f, 1f);
+			nodesArrayProp.GetArrayElementAtIndex(1).FindPropertyRelative("position").vector3Value = new Vector3(1f, 0f, 2f);
+			nodesArrayProp.GetArrayElementAtIndex(2).FindPropertyRelative("position").vector3Value = new Vector3(-1f, 0f, 4f);
+			nodesArrayProp.GetArrayElementAtIndex(3).FindPropertyRelative("position").vector3Value = new Vector3(0f, 0f, 5f);
 
 			serializedObject.ApplyModifiedProperties();
 		}
@@ -31,6 +31,8 @@ public class PathEditor : Editor
 
 	public override void OnInspectorGUI()
 	{
+		DrawDefaultInspector();
+
 		GUI.enabled = false;
 		EditorGUILayout.IntField("Number of segments", (nodesArrayProp.arraySize - 1) / 3);
 		GUI.enabled = true;
@@ -38,14 +40,14 @@ public class PathEditor : Editor
 		GUILayout.BeginVertical();
 		GUILayout.BeginHorizontal();
 
-		if (GUILayout.Button("Add"))
+		if(GUILayout.Button("Add"))
 		{
 			nodesArrayProp.arraySize += 3;
 
 			//Position of the end point of the path
 			Vector3 end = nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 4).FindPropertyRelative("position").vector3Value;
 
-			Vector3 endForward = Vector3.forward; //TODO: Get direction curve is facing
+			Vector3 endForward = path.GetNormal(path.length);
 
 			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 3).FindPropertyRelative("position").vector3Value = end + endForward * 1f;
 			nodesArrayProp.GetArrayElementAtIndex(nodesArrayProp.arraySize - 2).FindPropertyRelative("position").vector3Value = end + endForward * 3f;
@@ -55,7 +57,7 @@ public class PathEditor : Editor
 		{
 			GUI.enabled = false;
 		}
-		if (GUILayout.Button("Remove"))
+		if(GUILayout.Button("Remove"))
 		{
 			nodesArrayProp.arraySize -= 3;
 		}
@@ -73,7 +75,9 @@ public class PathEditor : Editor
 		{
 			SerializedProperty posProp = nodeProp.FindPropertyRelative("position");
 
-			posProp.vector3Value = Handles.PositionHandle(posProp.vector3Value, Quaternion.identity);
+			Vector3 worldPos = path.transform.TransformPoint(posProp.vector3Value);
+			worldPos = Handles.PositionHandle(worldPos, Quaternion.identity);
+			posProp.vector3Value = path.transform.InverseTransformPoint(worldPos);
 		}
 
 		serializedObject.ApplyModifiedProperties();
---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPath : MonoBehaviour
+{
+	public Path path;
+
+	public bool lockRotation = false;
+
+	public float speed = 3f;
+
+
+	public float distOnPath = 0f;
+
+	void OnValidate()
+	{
+		UpdateObject();
+
+		distOnPath = Mathf.Clamp(distOnPath, 0f, path.length);
+	}
+
+	void LateUpdate()
+	{
+		UpdateObject();
+
+		distOnPath = Mathf.Clamp(distOnPath + speed * Time.deltaTime, 0f, path.length);
+	}
+
+	public void UpdateObject()
+	{
+		transform.position = path.GetPos(distOnPath);
+
+		if(lockRotation)
+		{
+			transform.rotation = Quaternion.LookRotation(path.GetNormal(distOnPath));
+		}
+	}
+}
